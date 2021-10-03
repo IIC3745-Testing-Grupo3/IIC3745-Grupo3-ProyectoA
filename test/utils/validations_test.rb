c7ef49aa6cb5c1ca0ebeb2e087d346830c8b63c5
arpincheira <arpincheira@uc.cl>
2021-10-03 19:28:05 -0300

    refactor (input-bug-test): fix bundle exec rake offences

--- test/utils/validations_test.rb
+++ test/utils/validations_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../test_helper'
+require_relative '../../src/utils/validations'
 require 'test/unit'
 
 class ValidationsTest < Test::Unit::TestCase
