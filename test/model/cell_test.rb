e82bfe48a4ccda8b2efa9497bdcedb681e446b0f
meretamal <meretamal@uc.cl>
2021-10-02 21:16:23 -0300

    refactor(tests): add setup method

--- test/model/cell_test.rb
+++ test/model/cell_test.rb
@@ -5,21 +5,22 @@ require_relative '../../src/model/cell'
 require 'test/unit'
 
 class CellTest < Test::Unit::TestCase
+  def setup
+    @cell = Cell.new(0)
+  end
+
   def test_reveal
-    cell = Cell.new(0)
-    cell.reveal
-    assert_equal(cell.hidden, false)
+    @cell.reveal
+    assert_equal(@cell.hidden, false)
   end
 
   def test_to_s
-    cell = Cell.new(0)
-    assert_equal(cell.to_s, '-')
-    cell.reveal
-    assert_equal(cell.to_s, '0')
+    assert_equal(@cell.to_s, '-')
+    @cell.reveal
+    assert_equal(@cell.to_s, '0')
   end
 
   def test_inspect
-    cell = Cell.new(0)
-    assert_equal(cell.inspect, '-')
+    assert_equal(@cell.inspect, '-')
   end
 end
